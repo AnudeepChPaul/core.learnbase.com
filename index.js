6033a6047ec3445d6c01888f7632dc59ffe4b3ef
Anudeep Chandra Paul <achandrapaul@manh.com>
2020-05-27 04:50:20 +0530

    changes

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 'use strict'
+const env = require('./lib/Env')
 
-module.exports = { 
-    Core: require('./lib/Env')
+
+module.exports = function() { 
+    return {
+        Core: {
+            HOME_PAGE_URL: "https://app.learnbase.anudeepchpaul.in",
+            AUTH_PAGE_URL: "https://auth.learnbase.anudeepchpaul.in"
+        }
+    }
 }
