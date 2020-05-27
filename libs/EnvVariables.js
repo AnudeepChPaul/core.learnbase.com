e31f7f4488ce643e6ad897308374ae861df0f86b
Anudeep Chandra Paul <achandrapaul@manh.com>
2020-05-27 13:24:54 +0530

    Adding method

--- libs/EnvVariables.js
+++ libs/EnvVariables.js
@@ -1,8 +1,11 @@
 "use strict";
 
+const envVars = (name) => process.env[name] || null;
+
 module.exports = (function () {
   return {
     HOME_PAGE_URL: "https://app.learnbase.anudeepchpaul.in",
     AUTH_PAGE_URL: "https://auth.learnbase.anudeepchpaul.in",
+    envVars: envVars
   };
 })();
