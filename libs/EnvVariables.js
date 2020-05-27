00939d1c6e0afc963b880b87b4d6715dd4a749d6
Anudeep Chandra Paul <achandrapaul@manh.com>
2020-05-27 13:33:51 +0530

    method name change

--- libs/EnvVariables.js
+++ libs/EnvVariables.js
@@ -6,6 +6,6 @@ module.exports = (function () {
   return {
     HOME_PAGE_URL: "https://app.learnbase.anudeepchpaul.in",
     AUTH_PAGE_URL: "https://auth.learnbase.anudeepchpaul.in",
-    envVars: envVars
+    getEnvironmentVariables: envVars
   };
 })();
